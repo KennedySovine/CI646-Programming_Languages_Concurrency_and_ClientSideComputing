a8ad58cb3831d8f6ad754de437ba233272b1061a
KennedySovine <89476714+KennedySovine@users.noreply.github.com>
2025-02-21 09:59:06 +0000

    Update HandTrackingExampleApp.java

--- CI646Maven/src/main/java/org/example/HandTrackingExampleApp.java
+++ CI646Maven/src/main/java/org/example/HandTrackingExampleApp.java
@@ -11,13 +11,17 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 public class HandTrackingExampleApp extends GameApplication {
 
     private Canvas canvas;
     private GraphicsContext g;
+    private int distanceMax = 0;
+
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -36,6 +40,8 @@ public class HandTrackingExampleApp extends GameApplication {
         canvas = new Canvas(800, 600);
         g = canvas.getGraphicsContext2D();
         g.setFill(Color.BLUE);
+        g.setFill(Color.BLUE);
+        g.setStroke(Color.RED); // Set the color for the lines
 
         FXGL.addUINode(canvas);
 
@@ -44,25 +50,53 @@ public class HandTrackingExampleApp extends GameApplication {
         FXGL.getService(HandTrackingService.class)
                 .addInputHandler(hand -> {
                     // code
-                    System.out.println(hand);
+                    //System.out.println(hand);
 
                     Platform.runLater(() -> {
-                        //g.clearRect(0, 0, 800, 600);
+                        g.clearRect(0, 0, 800, 600);
 
-                        Point3D p = hand.getPoints().get(0);
+                        List<Point3D> points = hand.getPoints();
+                        for (int i = 0; i < points.size() - 1; i++) {
 
-                        g.fillOval((1 - p.getX()) * 1800 - 400, p.getY() * 1600 - 400, 10, 10);
+                            Point3D p = hand.getPoints().get(i);
 
-//                        Stage stage = FXGL.getPrimaryStage();
-//                        stage.setX((1 - point.getX()) * 1800 - 400);
+                            //System.out.println(p);
 
-//                        hand.getPoints().forEach(p -> {
-//
-//                            g.fillOval((1 - p.getX()) * 1800 - 400, p.getY() * 1600 - 400, 10, 10);
-//                        });
+                            g.fillOval((1 - p.getX()) * 900 - 300, p.getY() * 800 - 300, 10, 10);
+
+                            Stage stage = FXGL.getPrimaryStage();
+                            //stage.setX((1 - point.getX()) * 1800 - 400);
+
+                            //2 - 5 is thumb
+                            //if (i > 0){
+                            //Thumb, index, middle, ring, pinky
+                            if ((i > 0 &&  i < 5) || (i > 5 && i < 9) || (i > 9 && i < 13) || (i > 13 && i < 17) || (i > 17 && i < 20)) {
+                                Point3D prev = hand.getPoints().get(i - 1);
+                                System.out.println((p.getX() - prev.getX()) / (p.getY() - prev.getY()));
+                                g.strokeLine((1 - prev.getX()) * 900 - 300, prev.getY() * 800 - 300,
+                                        (1 - p.getX()) * 900 - 300, p.getY() * 800 - 300);
+                                //hand.getPoints().forEach(o -> {
+
+                                //g.fillOval((1 - o.getX()) * 900 - 300, o.getY() * 800 - 300, 20, 20);
+                            }
+                            //Other lines
+                            if (i == 1){
+                                Point3D prev = hand.getPoints().get(0);
+                                g.strokeLine((1 - prev.getX()) * 900 - 300, prev.getY() * 800 - 300,
+                                        (1 - p.getX()) * 900 - 300, p.getY() * 800 - 300);
+                            }
+
+                            /*int[] Knuckles = {5, 9, 13, 17};
+                            for (int j = 0; j < Knuckles.length - 1; j++) {
+                                if (i == Knuckles[j]){
+                                    Point3D next = hand.getPoints().get(j+1);
+                                    g.strokeLine((1 - next.getX()) * 900 - 300, next.getY() * 800 - 300,
+                                            (1 - p.getX()) * 900 - 300, p.getY() * 800 - 300);
+                                }
+                            }*/
+                        }
                     });
-
-                 });
+                });
         
         FXGL.getService(HandTrackingService.class)
                 .readyProperty()
